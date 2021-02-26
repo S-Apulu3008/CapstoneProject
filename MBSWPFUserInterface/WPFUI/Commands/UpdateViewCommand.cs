2a4573839d4afed81fa7e533333960ed127ee229
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2021-02-25 21:33:09 -0500

    The start of the our user interface.
    Unfortunately I could not finish the login page at the time. Was still figuring out how to switch views within the shell.
    
    I implemented the Models-Views-ViewModels design format. Still fairly new to how it works but so far everything works well.

---
+++ MBSWPFUserInterface/WPFUI/Commands/UpdateViewCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFUI.ViewModels;
+
+namespace WPFUI.Commands
+{
+    public class UpdateViewCommand : ICommand
+    {
+        private MainViewModel viewModel;
+
+        public UpdateViewCommand(MainViewModel viewModel)
+        {
+            this.viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (parameter.ToString() == "Login")
+            {
+                viewModel.SelectedViewModel = new LoginViewModel();
+            }
+            else if (parameter.ToString() == "Manage Account") 
+            {
+                viewModel.SelectedViewModel = new ManageAccountViewModel();
+            }
+        }
+    }
+}
