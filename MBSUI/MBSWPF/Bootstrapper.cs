57b994a283107fdb10c393959a97c0349cc9a428
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2021-03-25 13:41:06 -0400

    Learning Caliburn.Micro

---
+++ MBSUI/MBSWPF/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using MBSWPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MBSWPF
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        //Override the base startip from bootstrapperbase
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
