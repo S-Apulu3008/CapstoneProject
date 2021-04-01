954c52bf177ef441768e3ae619dc1ffe310f3370
Quentin-Merritt <38696938+Quentin-Merritt@users.noreply.github.com>
2021-04-01 16:00:22 -0400

    Switching through the views using Caliburn Micro
    Have to add the database back to the program.

--- MBSUI/MBSWPF/ViewModels/ShellViewModel.cs
+++ MBSUI/MBSWPF/ViewModels/ShellViewModel.cs
@@ -10,6 +10,28 @@ namespace MBSWPF.ViewModels
     //Screen allows more control to open and close views.
     public class ShellViewModel : Conductor<object>
     {
+        private LoginViewModel _loginVM;
+        
+        //Load login when shell view starts
+        //Getting a login viewmodel and storing it in a private variable
+        public ShellViewModel(LoginViewModel loginVM)
+        {
+            //Constructor injection to pass in an instance then activate to use later.
+            _loginVM = loginVM;
+            //Load the login form on the shell viem model
+            ActivateItem(_loginVM);
+        }
 
+        //Create a new instance of the home view model. 
+        public void LoadHomeView() 
+        {
+            ActivateItem(new HomeViewModel());
+        }
+
+        //Create a new instance of the register view model
+        public void LoadAccountView() 
+        {
+            ActivateItem(new AccountViewModel());
+        }
     }
 }
