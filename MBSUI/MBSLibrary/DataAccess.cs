4f6aad3a27d0999d4bf0145ad63424dc297fa9e6
daltonb3657 <daltonb3657@student.faytechcc.edu>
2021-05-07 13:43:29 -0400

    added data access for products

--- MBSUI/MBSLibrary/DataAccess.cs
+++ MBSUI/MBSLibrary/DataAccess.cs
@@ -26,5 +26,42 @@ namespace MBSLibrary
                 connection.Execute("dbo.AddEmployee", employee, commandType: CommandType.StoredProcedure);
             }
         }
+
+        public List<Product> GetProductByName(string name)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("MBSDatabase")))
+            {
+                var output = connection.Query<Product>($"dbo.GetProductsByName @Name", new { Name = name }).ToList();
+                return output;
+            }
+        }
+
+        public List<Product> GetProductByGenre(string genre)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("MBSDatabase")))
+            {
+                var output = connection.Query<Product>($"dbo.GetProductsByGenre @Genre", new { Genre = genre }).ToList();
+                return output;
+            }
+        }
+
+        public List<Product> GetProductBySKU(int sku)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("MBSDatabase")))
+            {
+                var output = connection.Query<Product>($"dbo.GetProductsBySKU @SKU", new { SKU = sku }).ToList();
+                return output;
+            }
+        }
+
+        public List<Product> GetReorderedItems()
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("MBSDatabase")))
+            {
+                var output = connection.Query<Product>("dbo.GetReorderItems").ToList();
+                return output;
+            }
+        }
+
     }
 }
---
+++ MBSUI/MBSLibrary/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBSLibrary
+{
+    public class Product
+    {
+        public int SKU { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+        public int Units_On_Hand { get; set; }
+    }
+}
