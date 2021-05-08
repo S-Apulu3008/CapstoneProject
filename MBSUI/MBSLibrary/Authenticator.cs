54b1394056b4722182a59dd26f7bdd2d3d134a3c
daltonb3657 <daltonb3657@student.faytechcc.edu>
2021-05-08 18:45:49 -0400

    added password verification

--- MBSUI/MBSLibrary/Authenticator.cs
+++ MBSUI/MBSLibrary/Authenticator.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 
 namespace MBSLibrary
 {
     public class Authenticator
     {
         DataAccess db = new DataAccess();
+        IPasswordHasher hasher = new PasswordHasher();
         public Employee Login(string employeeId, string password)
         {
             List<Employee> employees = db.GetEmployees(employeeId);
 
+            PasswordVerificationResult passwordResult = hasher.VerifyHashedPassword(employees[0].Password, password);
+
+            if (passwordResult != PasswordVerificationResult.Success)
+            {
+                throw new Exception();
+            }
             return employees[0];
         }
     }
